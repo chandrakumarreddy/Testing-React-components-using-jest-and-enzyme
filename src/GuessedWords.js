520672e61b1d980dbac045097cfef58f00e26f76
chandrakumarreddy <chandrapenugonda655@gmail.com>
2019-01-21 08:51:05 +0530

    gussed word component test

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -1,8 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const GuessedWords = props => {
-	return <div />;
+const GuessedWords = ({ guessedWords }) => {
+	const renderGuessedWords = guessedWords.map((guess, index) => (
+		<tr data-test="guessedword" key={index}>
+			<td>{guess.word}</td>
+			<td>{guess.wordlength}</td>
+		</tr>
+	));
+	let content =
+		guessedWords.length === 0 ? (
+			<span data-test="guessed-instructions">Try to guess the word</span>
+		) : (
+			<table data-test="guessedWords">
+				<thead>
+					<tr>
+						<th>Guess</th>
+						<th>word length</th>
+					</tr>
+				</thead>
+				<tbody>{renderGuessedWords}</tbody>
+			</table>
+		);
+	return <div data-test="component-guessed-word">{content}</div>;
 };
 
 GuessedWords.propTypes = {
--- src/__tests__/GuessedWords.tests.js
+++ src/__tests__/GuessedWords.tests.js
@@ -22,4 +22,45 @@ describe("<GussedWords> component", () => {
 		const validatingProps = validatePropTypes(GuessedWords, defaultProps);
 		expect(validatingProps).toBeUndefined();
 	});
+
+	describe("if there are no gussed words", () => {
+		let wrapper;
+		beforeEach(() => {
+			wrapper = setup({ guessedWords: [] });
+		});
+		test("renders guessed component", () => {
+			const guessedComponent = findByAttr(
+				wrapper,
+				"component-guessed-word"
+			);
+			expect(guessedComponent.length).toBe(1);
+		});
+		test("renders guessed Instructions", () => {
+			const Instructions = findByAttr(wrapper, "guessed-instructions");
+			expect(Instructions.text().length).not.toBe(0);
+		});
+	});
+
+	describe("if there are guessed words", () => {
+		let wrapper;
+		beforeEach(() => {
+			const guessedWords = [
+				{ word: "train", wordlength: 5 },
+				{ word: "bus", wordlength: 3 }
+			];
+			wrapper = setup({ guessedWords });
+		});
+		test("renders GuessedWords component", () => {
+			const component = findByAttr(wrapper, "component-guessed-word");
+			expect(component.length).toBe(1);
+		});
+		test("renders guessed words table", () => {
+			const guessedWordsTable = findByAttr(wrapper, "guessedWords");
+			expect(guessedWordsTable.length).toBe(1);
+		});
+		test("guessed words count to be 2", () => {
+			const guessedword = findByAttr(wrapper, "guessedword");
+			expect(guessedword.length).toBe(2);
+		});
+	});
 });
